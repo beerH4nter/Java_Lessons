8b6905dc79b190962bf1168a526d8345d8dfc9ec
GameOverVN <krutoy.fox.reserve@mail.ru>
2024-03-10 16:02:14 +0300

    second lesson

--- sec_lesson/src/Main.java
+++ sec_lesson/src/Main.java
@@ -1,8 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int x = 10;
+        int y = 20;
+        System.out.println("x = " + x + " y = " + y);
+
+        int buff;
+        buff = x;
+        x = y;
+        y = buff;
+        System.out.println("After changes: x = " + x + " y = " + y);
+
+        x = 7;
+        y = 5;
+        System.out.println("x = " + x + " y = " + y);
+        x = y;
+        y = 7;
+        System.out.println("After changes: x = " + x + " y = " + y);
+
+        x = 20;
+        y = 30;
+        System.out.println("x = " + x + " y = " + y);
+        x = y + 5;
+        y = x + 10;
+        System.out.println("After changes: x = " + x + " y = " + y);
+
+
+
+
+
 
     }
 }
