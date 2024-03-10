aaddf432d74e5ffd9e29f2649e1c54b26cab390a
GameOverVN <krutoy.fox.reserve@mail.ru>
2024-03-10 13:22:14 +0300

    start

---
+++ first_lesson/src/Main.java
@@ -0,0 +1,102 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello\\world");
+        System.out.println("Hello world");
+        System.out.println("Artyom Bokov");
+        System.out.print(0/10 + "\n");
+
+        System.out.println("*\n" + "**\n" + "***\n" + "****\n" + "*****\n"
+                + "******\n" + "*******\n");
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+        System.out.println("*******\n");
+
+        System.out.println("*******");
+        System.out.println("******");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*\n");
+
+        System.out.println("*");
+        System.out.println(" *");
+        System.out.println("  *");
+        System.out.println("   *");
+        System.out.println("    *");
+        System.out.println("     *");
+        System.out.println("      *\n");
+
+
+        System.out.println("      *");
+        System.out.println("     *");
+        System.out.println("    *");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*\n");
+
+
+        System.out.println("*****\n" + "*****\n" + "*****\n" + "*****\n" +
+                "*****\n" + "*****\n" + "*****\n" + "*****\n");
+
+
+        System.out.println("       *       ");
+        System.out.println("      ***      ");
+        System.out.println("     *****     ");
+        System.out.println("    *******    ");
+        System.out.println("   *********   ");
+        System.out.println("  ***********  ");
+        System.out.println(" ************* ");
+        System.out.println("***************\n");
+
+        System.out.println("\t\t\t*  *");
+        System.out.println("\t   *\t\t\t *");
+        System.out.println("\t*\t\t\t\t\t*\n");
+        System.out.println("*\t\t\t\t\t\t\t*");
+        System.out.println("*\t\t\t\t\t\t\t*\n");
+        System.out.println("\t*\t\t\t\t\t*");
+        System.out.println("\t   *\t\t\t *");
+        System.out.println("\t\t\t*  *\n");
+
+        System.out.println("************\n" + "*          *\n" +
+                "*          *\n" + "*  ******  *\n" + "*  *    *  *\n" +
+                "*  *    *  *\n" + "*  *    *  *\n" + "*  *    *  *\n" +
+                "*  ******  *\n" + "*          *\n" +
+                "*          *\n" + "************\n");
+
+        System.out.println("        *  *");
+        System.out.println("     *        *");
+        System.out.println("   *   *******   *");
+        System.out.println(" *     *  *  *     *");
+        System.out.println("*      * * * *      *");
+        System.out.println(" *     **   **     *");
+        System.out.println("   *   *******   *");
+        System.out.println("      *       *");
+        System.out.println("         *  *\n");
+
+        System.out.println("       *");
+        System.out.println("      ***");
+        System.out.println("     *****");
+        System.out.println("    *******");
+        System.out.println("   *********");
+        System.out.println("  ***********");
+        System.out.println(" *************");
+        System.out.println("***************");
+        System.out.println(" *************");
+        System.out.println("  ***********");
+        System.out.println("   *********");
+        System.out.println("    *******");
+        System.out.println("     *****");
+        System.out.println("      ***");
+        System.out.println("       *");
+
+    }
+}
