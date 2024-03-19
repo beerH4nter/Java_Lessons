3fa072d27cb033cc20c1efcb78e91ef35b68b74e
GameOverVN <krutoy.fox.reserve@mail.ru>
2024-03-19 11:32:25 +0300

    fourth lesson after homework

--- fourth_lesson/src/Main.java
+++ fourth_lesson/src/Main.java
@@ -6,30 +6,89 @@ public class Main
     public static void main(String[] args)
     {
 
-        System.out.println("Start\n");
-        int a = 50;
+        int a = 20;
+        int b = 2;
+        int c = 15;
 
-        if(a > 0)
+        if(a > b && b > c)
         {
-            System.out.println("Positive");
+            System.out.println("Max: " + a);
+            System.out.println("Min: " + c);
         }
-        if(a < 0)
+        else if (a > c && c > b)
         {
-            System.out.println("Negative");
+            System.out.println("Max: " + a);
+            System.out.println("Min: " + b);
         }
-
-        boolean isMan = true;
-        int age = 18;
-        if (isMan && age >= 18)
+        else if (b > a && a > c)
         {
-            System.out.println("\nWelcom to army");
+            System.out.println("Max: " + b);
+            System.out.println("Min: " + c);
+        }
+        else if (b > c && c > a)
+        {
+            System.out.println("Max: " + b);
+            System.out.println("Min: " + a);
+        }
+        else if (c > b && b > a)
+        {
+            System.out.println("Max: " + c);
+            System.out.println("Min: " + a);
         }
         else
         {
-            System.out.println("\nYou are lucky, but not for a long time");
+            System.out.println("Max: " + c);
+            System.out.println("Min: " + b);
         }
 
-        System.out.println("\nFinish");
-        
+        a = -27;
+
+        if (a > 0 && a != 0)
+        {
+            System.out.println(a + " is positive");
+
+            if(a % 2 == 0 && a % 5 == 0)
+            {
+                System.out.println(a + " is odd and multiple of five");
+            }
+            else if (a % 2 == 0 && a % 5 != 0)
+            {
+                System.out.println(a + " is odd and not multiple of five");
+            }
+            else if (a % 2 != 0 && a % 5 == 0)
+            {
+                System.out.println(a + " is even and multiple of five");
+            }
+            else
+            {
+                System.out.println(a + " is even and not multiple of five");
+            }
+        }
+        else if(a < 0 && a != 0)
+        {
+            System.out.println(a + " is negative");
+
+            if(a % 2 == 0 && a % 5 == 0)
+            {
+                System.out.println(a + " is odd and multiple of five");
+            }
+            else if (a % 2 == 0 && a % 5 != 0)
+            {
+                System.out.println(a + " is odd and not multiple of five");
+            }
+            else if (a % 2 != 0 && a % 5 == 0)
+            {
+                System.out.println(a + " is even and multiple of five");
+            }
+            else
+            {
+                System.out.println(a + " is even and not multiple of five");
+            }
+        }
+        else
+        {
+            System.out.println(a + " is equal zero");
+        }
     }
+
 }
