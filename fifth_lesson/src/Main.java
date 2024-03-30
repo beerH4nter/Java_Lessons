e81463b5a11a2732c68aa9fd38377ed4e72a9b13
GameOverVN <krutoy.fox.reserve@mail.ru>
2024-03-30 15:51:52 +0300

    seventh lesson

--- fifth_lesson/src/Main.java
+++ fifth_lesson/src/Main.java
@@ -10,95 +10,6 @@ public class Main
         Scanner scanner = new Scanner(System.in);
 
 
-        System.out.println("Enter first number:");
-        int first = scanner.nextInt();
-        System.out.println("Enter second number");
-        int sec = scanner.nextInt();
-
-        System.out.println("Enter what you want to do with this numbers(+, -, *, /)");
-        String sign = scanner.next();
-
-        switch (sign)
-        {
-            case "+":
-                System.out.println("Your answer: " + (first+sec));
-                break;
-            case "-":
-                System.out.println("Your answer: " + (first - sec));
-                break;
-
-            case "*":
-                System.out.println("Your answer: " + (first*sec));
-                break;
-
-            case "/":
-                if(sec != 0)
-                {
-                    System.out.println("Your answer: " + (first/sec));
-                }
-                else
-                {
-                    System.out.println("NONE");
-                }
-                break;
-            default:
-                System.out.println("Wrong sign!");
-                break;
-        }
-
-
-        System.out.println("Enter language (ru/en)");
-        String lang = scanner.next();
-        System.out.println("Enter word:");
-        String word = scanner.next();
-
-        switch (lang)
-        {
-            case "ru":
-                switch (word)
-                {
-                    case "кот":
-                        System.out.println("cat");
-                        break;
-                    case "собака":
-                        System.out.println("dog");
-                        break;
-                    case "птица":
-                        System.out.println("bird");
-                        break;
-                    default:
-                        System.out.println("Our dictionary is small, sorry");
-                        break;
-                }
-                break;
-
-            case "en":
-
-                switch (word)
-                {
-                    case "cat":
-                        System.out.println("кот");
-                        break;
-                    case "dog":
-                        System.out.println("собака");
-                        break;
-                    case "bird":
-                        System.out.println("птица");
-                        break;
-                    default:
-                        System.out.println("Our dictionary is small, sorry");
-                        break;
-
-                }
-
-                break;
-
-            default:
-                System.out.println("Wrong language!");
-                break;
-        }
-
-
         System.out.println("Enter unit of measurement of yor number(sm, mm, m, t, kg, g):");
         String firstMeasure = scanner.next();
         System.out.println("Enter number you want to transfer:");
@@ -225,7 +136,7 @@ public class Main
 
         }
 
-        System.out.println("Your result: " + firstNum * transferValue);
+        System.out.println("Your result: " + firstNum * transferValue + secMeasure);
 
     }
 
